f54b36489a7fdc9e6432d2322d2fba3e2179cc67
extezgrosfe <ezekiel.grosfeld@digitalhouse.com>
2021-12-31 00:28:46 -0300

    remove output

--- dist/index.js
+++ dist/index.js
@@ -300,25 +300,41 @@ exports.getWorkspace = getWorkspace;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.formatOutput = void 0;
 const formatOutput = (output) => {
-    output = output.replace(/[\u001b\u009b][[()#;?](?:[0-9]{1,4}(?:;[0-9]{0,4}))?[0-9A-ORZcf-nqry=><]/g, '');
-    output = output.replace(/No changes. No objects need to be destroyed./g, '+ No changes. No objects need to be destroyed.');
-    output = output.replace(/No changes. Infrastructure is up-to-date./g, '+ No changes. Infrastructure is up-to-date.');
+    /* output = output.replace(
+        /[\u001b\u009b][[()#;?](?:[0-9]{1,4}(?:;[0-9]{0,4}))?[0-9A-ORZcf-nqry=><]/g,
+        ''
+    );
+    output = output.replace(
+        /No changes. No objects need to be destroyed./g,
+        '+ No changes. No objects need to be destroyed.'
+    );
+    output = output.replace(
+        /No changes. Infrastructure is up-to-date./g,
+        '+ No changes. Infrastructure is up-to-date.'
+    );
     output = output.replace(/Destroy complete!/g, '- Destroy complete!');
     output = output.replace(/Apply complete!/g, '+ Apply complete!');
-    output = output.replace(/No changes. Your infrastructure matches the configuration./g, '+ No changes. Your infrastructure matches the configuration.');
-    output = output.replace(/No changes. Your infrastructure still matches the configuration./g, '+ No changes. Your infrastructure still matches the configuration.');
+    output = output.replace(
+        /No changes. Your infrastructure matches the configuration./g,
+        '+ No changes. Your infrastructure matches the configuration.'
+    );
+    output = output.replace(
+        /No changes. Your infrastructure still matches the configuration./g,
+        '+ No changes. Your infrastructure still matches the configuration.'
+    );
     output = output.replace(/Refreshing state... /g, '');
-    output = output.replace(/Error:/g, '- Error:');
+    output = output.replace(/Error:/g, '- Error:'); */
     /* 	output = output.replace(/\  \-/g, '-');
     output = output.replace(/\  \+/g, '+');
     output = output.replace(/\  \~/g, '!'); */
-    output = output.replace(/----/g, '====');
+    /* 	output = output.replace(/----/g, '====');
     output = output.replace(/%0A/g, '\n\n');
+
     output = output.replace(/,/g, '');
     output = output.replace(/>/g, '');
     output = output.replace(/Feature:/g, '\n\n> Feature:');
     output = output.replace(/Failure:/g, '- Failure:');
-    output = output.replace(/Scenario:/g, '> Scenario:');
+    output = output.replace(/Scenario:/g, '> Scenario:'); */
     /* 	output = output.replace(/\s\s+[+]/g, '\n+');
     output = output.replace(/\s\s+[!]/g, '\n!');
     output = output.replace(/\s\s+[-]/g, '\n-');
--- src/utils/ouput.ts
+++ src/utils/ouput.ts
@@ -1,5 +1,5 @@
 export const formatOutput = (output: string): string => {
-	output = output.replace(
+	/* output = output.replace(
 		/[\u001b\u009b][[()#;?](?:[0-9]{1,4}(?:;[0-9]{0,4}))?[0-9A-ORZcf-nqry=><]/g,
 		''
 	);
@@ -22,18 +22,18 @@ export const formatOutput = (output: string): string => {
 		'+ No changes. Your infrastructure still matches the configuration.'
 	);
 	output = output.replace(/Refreshing state... /g, '');
-	output = output.replace(/Error:/g, '- Error:');
+	output = output.replace(/Error:/g, '- Error:'); */
 	/* 	output = output.replace(/\  \-/g, '-');
 	output = output.replace(/\  \+/g, '+');
 	output = output.replace(/\  \~/g, '!'); */
-	output = output.replace(/----/g, '====');
+	/* 	output = output.replace(/----/g, '====');
 	output = output.replace(/%0A/g, '\n\n');
 
 	output = output.replace(/,/g, '');
 	output = output.replace(/>/g, '');
 	output = output.replace(/Feature:/g, '\n\n> Feature:');
 	output = output.replace(/Failure:/g, '- Failure:');
-	output = output.replace(/Scenario:/g, '> Scenario:');
+	output = output.replace(/Scenario:/g, '> Scenario:'); */
 	/* 	output = output.replace(/\s\s+[+]/g, '\n+');
 	output = output.replace(/\s\s+[!]/g, '\n!');
 	output = output.replace(/\s\s+[-]/g, '\n-');
